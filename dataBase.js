468eb8b5bc5636c10c524e6b521efbb8162441bc
mendi weissman <770mmw@gmail.com>
2021-11-17 13:48:58 +0200

    3

--- dataBase.js
+++ dataBase.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/finalProiect1', { useUnifiedTopology: true, useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost:27017/finalProiect1', { useUnifiedTopology: true, useNewUrlParser: true });
